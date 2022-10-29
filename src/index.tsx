cb8f280f8116d4974df73120cef4375b6877bcbf
NicoAnabalon <nicolas.anabalon.a@gmail.com>
2022-10-29 01:48:46 -0300

    Remove basename in HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
