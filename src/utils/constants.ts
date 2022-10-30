643b1786726ba77178242aed6aa5772a98f03f10
NicoAnabalon <nicolas.anabalon.a@gmail.com>
2022-10-30 19:16:17 -0300

    Add contact me view

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,8 @@
 export const LINKEDIN_URL =
   "https://www.linkedin.com/in/nicol%C3%A1s-felipe-anabal%C3%B3n-abarca/";
 export const GITHUB_URL = "https://github.com/NicoAnabalon";
+// eslint-disable-next-line
+export const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const serviceID = process.env.REACT_APP_SERVICE_ID ?? "";
+export const templateID = process.env.REACT_APP_TEMPLATE_ID ?? "";
+export const publicKey = process.env.REACT_APP_PUBLIC_KEY ?? "";
--- src/views/ContactMe.tsx
+++ src/views/ContactMe.tsx
@@ -1,7 +1,164 @@
+import { ReportProblem } from "@mui/icons-material";
+import {
+  Alert,
+  Box,
+  Button,
+  Paper,
+  Snackbar,
+  TextField,
+  Tooltip,
+} from "@mui/material";
+import { useRef, useState } from "react";
+import { useForm, FieldValues } from "react-hook-form";
+import { Container } from "../components";
+import { EMAIL_REGEX, publicKey, serviceID, templateID } from "../utils";
+import emailjs from "@emailjs/browser";
+import "./styles.scss";
+
 interface IContactMe {
   isMobile: boolean;
 }
 
+interface IContactForm {
+  from_name: string;
+  to_name: string;
+  reply_to: string;
+  message: string;
+}
+
 export const ContactMe = ({ isMobile }: IContactMe) => {
-  return <>To be implemented</>;
+  const formRef = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IContactForm>();
+  const [sendingEmail, setSendingEmail] = useState<boolean>(false);
+  const [alert, setAlert] = useState<{
+    severity: "success" | "error";
+    message: string;
+  } | null>(null);
+
+  const handleSendEmail = async (payload: FieldValues) => {
+    const { from_name, to_name = "NicoAnabalon", reply_to, message } = payload;
+    setSendingEmail(true);
+    setAlert(null);
+    try {
+      await emailjs
+        .send(
+          serviceID,
+          templateID,
+          {
+            from_name,
+            to_name,
+            reply_to,
+            message,
+          },
+          publicKey
+        )
+        .then(
+          () => {
+            setAlert({
+              severity: "success",
+              message: "Email sent successfully",
+            });
+            reset();
+          },
+          () => {
+            setAlert({
+              severity: "error",
+              message: "There was an error sending the email. Please try again",
+            });
+          }
+        );
+    } catch {
+      setAlert({
+        severity: "error",
+        message: "There was an error sending the email. Please try again",
+      });
+    }
+    setSendingEmail(false);
+  };
+
+  const handleCloseSnackbar = () => {
+    setAlert(null);
+  };
+
+  return (
+    <Container
+      isMobile={isMobile}
+      children={
+        <Box component="form" className="contact-form" ref={formRef}>
+          {alert && (
+            <Snackbar
+              open={!!alert}
+              autoHideDuration={3000}
+              onClose={handleCloseSnackbar}
+            >
+              <Alert onClose={handleCloseSnackbar} severity={alert.severity}>
+                {alert.message}
+              </Alert>
+            </Snackbar>
+          )}
+          <TextField
+            className="text-field"
+            id="name"
+            label="Name"
+            InputProps={{
+              endAdornment: errors.from_name ? (
+                <Tooltip title={errors.from_name.message ?? "Required field"}>
+                  <ReportProblem className="required-field" />
+                </Tooltip>
+              ) : null,
+            }}
+            {...register("from_name", {
+              required: "Name is required",
+            })}
+          />
+          <TextField
+            className="text-field"
+            id="email"
+            label="Email"
+            InputProps={{
+              endAdornment: errors.reply_to ? (
+                <Tooltip title={errors.reply_to.message ?? "Required field"}>
+                  <ReportProblem className="required-field" />
+                </Tooltip>
+              ) : null,
+            }}
+            {...register("reply_to", {
+              required: "Email is required",
+              pattern: { value: EMAIL_REGEX, message: "Email is invalid" },
+            })}
+          />
+          <TextField
+            className="text-field message"
+            id="message"
+            label="Message"
+            multiline
+            InputProps={{
+              endAdornment: errors.message ? (
+                <Tooltip title={errors.message.message ?? "Required field"}>
+                  <ReportProblem className="required-field" />
+                </Tooltip>
+              ) : null,
+            }}
+            {...register("message", {
+              required: "Message is required",
+            })}
+          />
+          <Button
+            variant="outlined"
+            type="submit"
+            onClick={handleSubmit(handleSendEmail)}
+            disabled={sendingEmail}
+            className="submit-button"
+          >
+            {sendingEmail ? "Sending..." : "Send Email"}
+          </Button>
+        </Box>
+      }
+    />
+  );
 };
